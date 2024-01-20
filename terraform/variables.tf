9b7bd44a37222b2f180a4d965baa2807f8762062
Maxime Daraiche <maxiscoding@gmail.com>
2024-01-20 09:38:15 -0500

    Added variables definition

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "env_short" {
+    description = "Environment that we're deploying to (prod, dev, etc)"
+}
+
+variable "env_full" {
+    description = "Environment that we're deploying to (prod, dev, etc)"
+}
+
+variable "servers" {
+    description = "List of servers to be deployed"
+}
