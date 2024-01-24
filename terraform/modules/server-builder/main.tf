61a568713be159313d353c324fa0c88da860ef1b
Maxime Daraiche <maxiscoding@gmail.com>
2024-01-23 19:20:49 -0500

    Removed ansible_host resource since we are using a module for it now / added ansible_group as a local variable so we can retrieve it from other modules

--- terraform/modules/server-builder/main.tf
+++ terraform/modules/server-builder/main.tf
@@ -1,3 +1,7 @@
+locals {
+  ansible_group = var.ansible_group
+}
+
 #--------------------------------------------------------------------
 # Libvirt Volumes Definition
 #--------------------------------------------------------------------
@@ -23,18 +27,6 @@ resource "libvirt_cloudinit_disk" "cloudinit" {
     hostname = "${var.hostname}"
     domain   = "${var.domain}"
   })
-}
-
-#--------------------------------------------------------------------
-# Ansible Host Definition
-#--------------------------------------------------------------------
-resource "ansible_host" "host" {
-  name   = "${libvirt_domain.server.network_interface[0].addresses[0]}"
-  groups = [ "${var.ansible_group}" ]
-  
-  depends_on = [ 
-    libvirt_domain.server 
-  ]
 }
 
 #--------------------------------------------------------------------
