ab4829ebd629263f9c435acbfd2ffd7be5b09b38
Maxime Daraiche <maxiscoding@gmail.com>
2024-01-23 19:18:00 -0500

    Rewrote the ansible_hosts definition & removed ansible_playbook since that is not working

--- terraform/modules/ansible-manager/main.tf
+++ terraform/modules/ansible-manager/main.tf
@@ -1,20 +1,19 @@
 #--------------------------------------------------------------------
 # Ansible Host Definition
 #--------------------------------------------------------------------
-#resource "ansible_host" "host" {
-#  name   = "${libvirt_domain.server.network_interface[0].addresses[0]}"
-#  groups = [ "${var.ansible_group}" ]
-  
-#  depends_on = [ 
-#    libvirt_domain.server 
-#  ]
-#}
+resource "ansible_host" "host" {
+  for_each = var.ansible_hosts
+
+  name     = "${each.value.ip}"
+  groups   = [ "${each.value.ansible_group}" ]
+}
 
 #--------------------------------------------------------------------
 # Ansible Groups Definition
 #--------------------------------------------------------------------
 resource "ansible_group" "group" {
   count = length(var.ansible_groups)
+
   name  = "${var.ansible_groups[count.index]}"
 }
 
@@ -22,3 +21,11 @@ resource "ansible_group" "group" {
 # Ansible Playbooks Definition
 #--------------------------------------------------------------------
 
+# As of now, this isn't working due to bugs on the provider end..
+# Kepping it to revisit it in a couple weeks / months
+
+#resource "ansible_playbook" "playbook" {
+#  name     = "nfsserver"
+#  playbook = "../ansible/nfs-server.yml"
+#  groups   = [ "nfsserver" ]
+#}
